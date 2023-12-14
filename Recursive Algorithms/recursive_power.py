052511ce61441b5bef3a7ce644becc749f861f26
ariy4 <bahraini913.ab@gmail.com>
2023-12-14 10:40:06 +0330

    fixed recursive_power.py algorithm

--- Recursive Algorithms/recursive_power.py
+++ Recursive Algorithms/recursive_power.py
@@ -1,14 +1,17 @@
-def recursive_power(x, n) -> int:
-    if n == 0:
-        return 1
-    if n % 2 == 0:
-        return (recursive_power(x, n // 2)) ** 2
-    elif n % 2 == 1:
-        return x * recursive_power(x, n - 1)
+def recursive_power(base, power, module) -> int:
+    if power == 1:
+        return base % module
+    elif power % 2 == 0:
+        temp = recursive_power(base, power // 2, module) % module
+        return temp**2 % module
+    elif power % 2 == 1:
+        temp = recursive_power(base, power - 1, module) % module
+        return (base * temp) % module
 
 
-base = int(input('Enter your base number (x ** n % p) --> x: '))
-power = int(input('Enter your power number (x ** n % p) --> n: '))
-module = int(input('Enter your base number (x ** n % p) --> p: '))
+x = int(input('Enter your base number (x ** n % p) --> x: '))
+n = int(input('Enter your power number (x ** n % p) --> n: '))
+p = int(input('Enter your module number (x ** n % p) --> p: '))
+print(f'Remainder is: {recursive_power(x, n, p)}')
 
-print(f'Remainder is: {recursive_power(base, power) % module}')
+# print(f'Remainder is: {recursive_power(3, 50000000, 13)}')
