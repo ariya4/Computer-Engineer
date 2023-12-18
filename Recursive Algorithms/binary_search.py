1d8afbaf39090bdd43d7a1ae10e9a8f3639363db
ariy4 <bahraini913.ab@gmail.com>
2023-12-18 09:23:38 +0330

    printing output for more clarification

--- Recursive Algorithms/binary_search.py
+++ Recursive Algorithms/binary_search.py
@@ -1,26 +1,26 @@
-def binary_search(low, high, target):
+def binary_search(low, high, target, lst):
     print('In Function')
     if low > high:
-        print('Not Found')
+        return 'Not Found'
     else:
         mid = (low + high) // 2
-        if num_list[mid] == target:
-            print(f'\nFound the target!\n{target} -> {num_list.index(target) + 1}th item in your list')
-        elif num_list[mid] > target:
+        if lst[mid] == target:
+            return f'\nFound the target!\n{lst[mid]} -> {lst.index(target) + 1}th item in your list'
+        elif lst[mid] > target:
             high = mid - 1
-            binary_search(low, high, target)
-        elif num_list[mid] < target:
+            return binary_search(low, high, target, lst)
+        elif lst[mid] < target:
             low = mid + 1
-            binary_search(low, high, target)
+            return binary_search(low, high, target, lst)
 
 
 num_list = [10, 12, 13, 14, 18, 20, 25, 27, 30, 35, 40, 45]
 
 # if we get an unsorted list (by getting the list by an input), it will be sorted by the following line
-# num_list.sort()
+num_list.sort()
 
 print(num_list)
 x = int(input('What are you looking for? '))
 start_list = 0
 end_list = len(num_list) - 1
-binary_search(start_list, end_list, x)
+print(binary_search(start_list, end_list, x, num_list))
